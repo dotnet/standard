46fef3272569e56e1666d4c24ea7e9eac6fda4dd
Wes Haggard <Wes.Haggard@microsoft.com>
2016-09-01 14:07:26 -0700

    Add platforms directory and seed it with net461 and xamarin.ios

---
+++ platforms/xamarin.ios/seed.cmd
@@ -0,0 +1,3 @@
+@setlocal
+pushd \\fxcore\Platforms\ApiCatalog\Xamarin.iOS 4.2.0.584
+\\fxcore\tools\others\GenApi\genapi.exe -assembly "mscorlib.dll;System.dll;System.Core.dll;System.IO.Compression.dll;System.IO.Compression.FileSystem.dll;System.Net.dll;System.Net.Http.dll;System.Numerics.dll;System.Runtime.Serialization.dll;System.Xml.dll;System.Xml.Linq.dll" -out %~dp0 -excludeAttributesList %~dp0..\attributeExcludeList.txt
