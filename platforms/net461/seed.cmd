bbd299716faa18eec62b02cca87582f641344f4c
Wes Haggard <Wes.Haggard@microsoft.com>
2016-09-02 17:57:51 -0700

    Remove obsolete Peer networking from platforms
    The assembly System.Net is commonly mistaken to contain the actual
    networking APIs but instead it contains an obsolete Peer networking
    stack which we don't want in the standard. The networking APIs
    mostly live in system or system.net.http.

--- platforms/net461/seed.cmd
+++ platforms/net461/seed.cmd
@@ -1,3 +1,3 @@
 @setlocal
 pushd \\fxcore\Platforms\ApiCatalog\.NET Framework 4.6.1
-\\fxcore\tools\others\GenApi\genapi.exe -assembly "mscorlib.dll;System.dll;System.Core.dll;System.Drawing.dll;System.IO.Compression.dll;System.IO.Compression.FileSystem.dll;System.Net.dll;System.Net.Http.dll;System.Numerics.dll;System.Runtime.Serialization.dll;System.Xml.dll;System.Xml.Linq.dll" -out %~dp0 -excludeAttributesList %~dp0..\attributeExcludeList.txt
+\\fxcore\tools\others\GenApi\genapi.exe -assembly "mscorlib.dll;System.dll;System.Core.dll;System.Drawing.dll;System.IO.Compression.dll;System.IO.Compression.FileSystem.dll;System.Net.Http.dll;System.Numerics.dll;System.Runtime.Serialization.dll;System.Xml.dll;System.Xml.Linq.dll" -out %~dp0 -excludeAttributesList %~dp0..\attributeExcludeList.txt
