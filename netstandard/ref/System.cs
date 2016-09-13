c431e836c0f62a205cea2ca77ba8d376dd0d2c10
Wes Haggard <Wes.Haggard@microsoft.com>
2016-09-12 23:20:36 -0700

    Factor out Win32 Media APIs from netstandard ref

--- netstandard/ref/System.cs
+++ netstandard/ref/System.cs
@@ -4681,49 +4681,6 @@ namespace System.IO.Compression
         public override void Write(byte[] array, int offset, int count) { }
     }
 }
-namespace System.Media
-{
-    [System.ComponentModel.ToolboxItemAttribute(false)]
-    public partial class SoundPlayer : System.ComponentModel.Component, System.Runtime.Serialization.ISerializable
-    {
-        public SoundPlayer() { }
-        public SoundPlayer(System.IO.Stream stream) { }
-        protected SoundPlayer(System.Runtime.Serialization.SerializationInfo serializationInfo, System.Runtime.Serialization.StreamingContext context) { }
-        public SoundPlayer(string soundLocation) { }
-        public bool IsLoadCompleted { get { throw null; } }
-        public int LoadTimeout { get { throw null; } set { } }
-        public string SoundLocation { get { throw null; } set { } }
-        public System.IO.Stream Stream { get { throw null; } set { } }
-        public object Tag { get { throw null; } set { } }
-        public event System.ComponentModel.AsyncCompletedEventHandler LoadCompleted { add { } remove { } }
-        public event System.EventHandler SoundLocationChanged { add { } remove { } }
-        public event System.EventHandler StreamChanged { add { } remove { } }
-        public void Load() { }
-        public void LoadAsync() { }
-        protected virtual void OnLoadCompleted(System.ComponentModel.AsyncCompletedEventArgs e) { }
-        protected virtual void OnSoundLocationChanged(System.EventArgs e) { }
-        protected virtual void OnStreamChanged(System.EventArgs e) { }
-        public void Play() { }
-        public void PlayLooping() { }
-        public void PlaySync() { }
-        public void Stop() { }
-        void System.Runtime.Serialization.ISerializable.GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) { }
-    }
-    public partial class SystemSound
-    {
-        internal SystemSound() { }
-        public void Play() { }
-    }
-    public sealed partial class SystemSounds
-    {
-        internal SystemSounds() { }
-        public static System.Media.SystemSound Asterisk { get { throw null; } }
-        public static System.Media.SystemSound Beep { get { throw null; } }
-        public static System.Media.SystemSound Exclamation { get { throw null; } }
-        public static System.Media.SystemSound Hand { get { throw null; } }
-        public static System.Media.SystemSound Question { get { throw null; } }
-    }
-}
 namespace System.Net
 {
     public partial class AuthenticationManager
