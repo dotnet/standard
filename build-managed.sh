cb98a47c34f11451ebfec2932a61292d6591ce31
Bryan Arant <bryanar@microsoft.com>
2016-12-14 19:44:31 -0800

    cleanup

--- build-managed.sh
+++ build-managed.sh
@@ -2,55 +2,12 @@
 
 usage()
 {
-    echo "Usage: $0 [platform]"
-    echo
-    echo "platform can be: FreeBSD, Linux, NetBSD, OSX, Windows."
+    echo "Usage: call ./build.sh"
     echo
 }
 
 
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
 
-__UnprocessedBuildArgs=
-__TestNugetRuntimeId=
-__BuildOS=
-__TargetOS=
-
-
-while :; do
-    if [ $# -le 0 ]; then
-        break
-    fi
-
-    lowerI="$(echo $1 | awk '{print tolower($0)}')"
-    case $lowerI in
-        freebsd)
-            __TestNugetRuntimeId="$__TestNugetRuntimeId=osx.10.10-x64"
-            __BuildOS="$__BuildOS=freebsd"
-            __TargetOS="$__TargetOS=freebsd"
-            ;;
-        netbsd)
-            __TestNugetRuntimeId="$__TestNugetRuntimeId=osx.10.10-x64"
-            __BuildOS="$__BuildOS=netbsd"
-            __TargetOS="$__TargetOS=netbsd"
-            ;;
-        osx)
-            __TestNugetRuntimeId="$__TestNugetRuntimeId=osx.10.10-x64"
-            __BuildOS="$__BuildOS=osx"
-            __TargetOS="$__TargetOS=osx"
-            ;;
-        windows)
-            __TestNugetRuntimeId="$__TestNugetRuntimeId=win7-x64"
-            __BuildOS="$__BuildOS=windows_nt"
-            __TargetOS="$__TargetOS=Windows_NT"
-            ;;
-        *)
-            __UnprocessedBuildArgs="$__UnprocessedBuildArgs $1"
-    esac
-
-    shift
-done
-
-
-$__scriptpath/run.sh build-managed $__BuildOS $__TargetOS $__TestNugetRuntimeId $__UnprocessedBuildArgs
+$__scriptpath/run.sh build-managed 
 exit $?
--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 usage()
 {
     echo
-    echo "WIP."
-    echo "Hello."
+    echo "Use this script only for generic build instructions that apply for both native builds and managed builds."
     echo
 }
 
@@ -14,33 +13,7 @@ fi
 
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
 
-
-while :; do
-    if [ $# -le 0 ]; then
-        break
-    fi
-    
-    if [[ $1 == *"centos.7"* ]] ; then
-       __IsCentos=1
-    fi
-    
-    if [[ $1 == *"osx.10"* ]] ; then
-       __IsOSX=1
-    fi
-
-    lowerI="$(echo $1 | awk '{print tolower($0)}')"
-    case $lowerI in
-        useservergc)
-            __ServerGC=1
-            ;;
-        *)
-    __UnprocessedBuildArgs="$__UnprocessedBuildArgs $1"
-    esac
-
-    shift
-done
-
-"$__scriptpath/build-managed.sh" $__UnprocessedBuildArgs
+"$__scriptpath/build-managed.sh"
 
 if [ $? -ne 0 ];then
    exit 1
