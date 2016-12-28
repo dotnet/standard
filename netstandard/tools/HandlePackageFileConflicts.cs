9d0e5aa6bbd789b2ce7c23bd300b3b81cb0f18e0
Eric St. John <ericstj@microsoft.com>
2016-12-28 13:17:12 -0800

    Skip missing and mismatched files
    If a file is missing or only one side has a particular version, skip,
    allowing the conflict to remain.

--- netstandard/tools/HandlePackageFileConflicts.cs
+++ netstandard/tools/HandlePackageFileConflicts.cs
@@ -21,10 +21,9 @@ namespace Microsoft.DotNet.Build.Tasks
 
         [Output]
         public ITaskItem[] ConflictingReferences { get; set; }
+
         [Output]
-
         public ITaskItem[] ConflictingCopyLocalItems { get; set; }
-
 
         public override bool Execute()
         {
@@ -53,6 +52,13 @@ namespace Microsoft.DotNet.Build.Tasks
                     // a conflict was found, determine the winner.
                     var winner = HandleConflict(existingItem, item);
 
+                    if (winner == null)
+                    {
+                        // no winner, skip it.
+                        // don't add to conflict list and just use the existing item for future conflicts.
+                        continue;
+                    }
+
                     if (winner != existingItem)
                     {
                         // replace existing item and add it to conflict list.
@@ -72,6 +78,17 @@ namespace Microsoft.DotNet.Build.Tasks
             }
         }
 
+        private Version GetFileVersion(string sourcePath)
+        {
+            var fvi = FileVersionInfo.GetVersionInfo(sourcePath);
+
+            if (fvi != null)
+            {
+                return new Version(fvi.FileMajorPart, fvi.FileMinorPart, fvi.FileBuildPart, fvi.FilePrivatePart);
+            }
+
+            return null;
+        }
 
         private static string GetSourcePath(ITaskItem item)
         {
@@ -88,13 +105,14 @@ namespace Microsoft.DotNet.Build.Tasks
         static readonly string[] s_targetPathMetadata = new[] { "TargetPath", "Path" };
         private static string GetTargetPath(ITaskItem item)
         {
+            // first use TargetPath, then Path, then fallback to filename+extension alone
             foreach (var metadata in s_targetPathMetadata)
             {
-                var targetPath = item.GetMetadata("TargetPath");
+                var value = item.GetMetadata(metadata);
 
-                if (!String.IsNullOrWhiteSpace(targetPath))
+                if (!String.IsNullOrWhiteSpace(value))
                 {
-                    return targetPath.Replace('\\', '/');
+                    return value.Replace('\\', '/');
                 }
             }
 
@@ -102,7 +120,6 @@ namespace Microsoft.DotNet.Build.Tasks
 
             return Path.GetFileName(sourcePath);
         }
-
 
         private ITaskItem HandleConflict(ITaskItem item1, ITaskItem item2)
         {
@@ -114,41 +131,31 @@ namespace Microsoft.DotNet.Build.Tasks
             var exists1 = File.Exists(sourcePath1);
             var exists2 = File.Exists(sourcePath2);
 
-            if (!exists1 && !exists2)
+            if (!exists1 || !exists2)
             {
-                Log.LogMessage($"{conflictMessage}.  Could not determine winner because {sourcePath1} and {sourcePath2} do not exist.  Arbitrarily choosing {item1.ItemSpec}.");
-                return item1;
-            }
+                var fileMessage = !exists1 ?
+                                    !exists2 ? 
+                                      "both files do" :
+                                      $"{item1.ItemSpec} does" :
+                                  $"{item2.ItemSpec} does";
 
-            if (exists1 && !exists2)
-            {
-                Log.LogMessage($"{conflictMessage}.  Choosing {item1.ItemSpec} because {sourcePath1} exists and {sourcePath2} does not.");
-                return item1;
+                Log.LogMessage($"{conflictMessage}.  Could not determine winner because {fileMessage} not exist.");
+                return null;
             }
-
-            if (!exists1 && exists2)
-            {
-                Log.LogMessage($"{conflictMessage}.  Choosing {item2.ItemSpec} because {sourcePath2} exists and {sourcePath1} does not.");
-                return item2;
-            }
-
+            
             var assemblyVersion1 = GetAssemblyVersion(sourcePath1);
             var assemblyVersion2 = GetAssemblyVersion(sourcePath2);
 
-            if (assemblyVersion1 != null || assemblyVersion2 != null)
+            // if only one is missing version
+            if (assemblyVersion1 == null ^ assemblyVersion2 == null)
             {
-                if (assemblyVersion1 == null)
-                {
-                    Log.LogMessage($"{conflictMessage}.  Choosing {item2.ItemSpec} because {sourcePath1} had no AssemblyVersion and {sourcePath2} did.");
-                    return item2;
-                }
+                var nonAssembly = assemblyVersion1 == null ? item1.ItemSpec : item2.ItemSpec;
+                Log.LogMessage($"{conflictMessage}. Could not determine a winner because {nonAssembly} is not an assembly.");
+                return null;
+            }
 
-                if (assemblyVersion2 == null)
-                {
-                    Log.LogMessage($"{conflictMessage}.  Choosing {item1.ItemSpec} because {sourcePath2} had no AssemblyVersion and {sourcePath1} did.");
-                    return item1;
-                }
-
+            if (assemblyVersion1 != assemblyVersion2)
+            {
                 if (assemblyVersion1 > assemblyVersion2)
                 {
                     Log.LogMessage($"{conflictMessage}.  Choosing {item1.ItemSpec} because AssemblyVersion {assemblyVersion1} is greater than {assemblyVersion2}.");
@@ -160,55 +167,35 @@ namespace Microsoft.DotNet.Build.Tasks
                     Log.LogMessage($"{conflictMessage}.  Choosing {item2.ItemSpec} because AssemblyVersion {assemblyVersion2} is greater than {assemblyVersion1}.");
                     return item2;
                 }
-
-                // if (assemblyVersion1 == assemblyVersion2) continue
             }
 
             var fileVersion1 = GetFileVersion(sourcePath1);
             var fileVersion2 = GetFileVersion(sourcePath2);
 
-            if (fileVersion1 != null || fileVersion2 != null)
+            // if only one is missing version
+            if (fileVersion1 == null ^ fileVersion2 == null)
             {
-                if (fileVersion1 == null)
-                {
-                    Log.LogMessage($"{conflictMessage}.  Choosing {item2.ItemSpec} because {sourcePath1} had no AssemblyVersion and {sourcePath2} did.");
-                    return item2;
-                }
+                var nonVersion = fileVersion1 == null ? item1.ItemSpec : item2.ItemSpec;
+                Log.LogMessage($"{conflictMessage}. Could not determine a winner because {nonVersion} has no file version.");
+                return null;
+            }
 
-                if (fileVersion2 == null)
-                {
-                    Log.LogMessage($"{conflictMessage}.  Choosing {item1.ItemSpec} because {sourcePath2} had no AssemblyVersion and {sourcePath1} did.");
-                    return item1;
-                }
-
+            if (fileVersion1 != fileVersion2)
+            {
                 if (fileVersion1 > fileVersion2)
                 {
-                    Log.LogMessage($"{conflictMessage}.  Choosing {item1.ItemSpec} because FileVersion {fileVersion1} is greater than {fileVersion2}.");
+                    Log.LogMessage($"{conflictMessage}.  Choosing {item1.ItemSpec} because file version {fileVersion1} is greater than {fileVersion2}.");
                     return item1;
                 }
 
                 if (fileVersion2 > fileVersion1)
                 {
-                    Log.LogMessage($"{conflictMessage}.  Choosing {item2.ItemSpec} because FileVersion {fileVersion2} is greater than {fileVersion1}.");
+                    Log.LogMessage($"{conflictMessage}.  Choosing {item2.ItemSpec} because file version {fileVersion2} is greater than {fileVersion1}.");
                     return item2;
                 }
-
-                // if (fileVersion1 == fileVersion2) continue
             }
 
-            Log.LogMessage($"{conflictMessage}.  Could not determine winner due to equal File and Assembly versions.  Arbitrarily choosing {item1.ItemSpec}.");
-            return item1;
-        }
-
-        private Version GetFileVersion(string sourcePath)
-        {
-            var fvi = FileVersionInfo.GetVersionInfo(sourcePath);
-
-            if (fvi != null)
-            {
-                return new Version(fvi.FileMajorPart, fvi.FileMinorPart, fvi.FileBuildPart, fvi.FilePrivatePart);
-            }
-
+            Log.LogMessage($"{conflictMessage}.  Could not determine winner due to equal file and assembly versions.");
             return null;
         }
     }
