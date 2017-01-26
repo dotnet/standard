2109c6d6f99b5db131ea589f53f7a6381fc96e9b
Matt Ellis <matell@microsoft.com>
2017-01-26 15:24:54 -0800

    Fix some unix build issues.
    - Flow arguments from build.sh and build-managed.sh to run.sh (so you
      can pass -Release and get a release build).
    - Public sign instead of full sign when using the Open key
      on *NIX (where csc does not yet support full signing).
    - Fix casing issue in a .builds file.

--- build.sh
+++ build.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-usage()
-{
-    echo
-    echo "Use this script only for generic build instructions that apply for both native builds and managed builds."
-    echo
-}
-
-if [ "$1" == "-?" ]; then
-    usage
-fi
-
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
-
-"$__scriptpath/build-managed.sh"
-
-if [ $? -ne 0 ];then
-   exit 1
-fi
+"$__scriptpath/build-managed.sh" $@
+exit $?
