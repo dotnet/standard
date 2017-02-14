7cb56a1da48dcca7c6480d905b98f454620dfb63
Alexander Köplinger <alex.koeplinger@outlook.com>
2017-02-14 01:38:45 +0100

    Add System.Numerics.Vectors to Xamarin platforms

--- platforms/xamarin.android/seed.cmd
+++ platforms/xamarin.android/seed.cmd
@@ -1,6 +1,6 @@
 @setlocal
 
-set SeedAssemblies="mscorlib.dll;System.dll;System.Core.dll;System.ComponentModel.Composition.dll;System.Data.dll;System.IO.Compression.dll;System.IO.Compression.FileSystem.dll;System.Net.Http.dll;System.Numerics.dll;System.Runtime.Serialization.dll;System.Transactions.dll;System.Xml.dll;System.Xml.Linq.dll;System.Web.Services.dll"
+set SeedAssemblies="mscorlib.dll;System.dll;System.Core.dll;System.ComponentModel.Composition.dll;System.Data.dll;System.IO.Compression.dll;System.IO.Compression.FileSystem.dll;System.Net.Http.dll;System.Numerics.dll;System.Numerics.Vectors.dll;System.Runtime.Serialization.dll;System.Transactions.dll;System.Xml.dll;System.Xml.Linq.dll;System.Web.Services.dll"
 
 %~dp0..\..\Tools\dotnetcli\dotnet %~dp0..\..\Tools\GenAPI.exe -assembly %SeedAssemblies% -libPath "%ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\MonoAndroid\v1.0" -out %~dp0 -excludeAttributesList %~dp0..\attributeExcludeList.txt -headerFile %~dp0..\..\netstandard\ref\license-header.txt -excludeApiList %~dp0apiExcludeList.txt
 %~dp0..\..\Tools\dotnetcli\dotnet %~dp0..\..\Tools\GenAPI.exe -assembly Mono.Android.dll -libPath "%ProgramFiles(x86)%\Reference Assemblies\Microsoft\Framework\MonoAndroid\v7.1" -out %~dp0 -excludeAttributesList %~dp0..\attributeExcludeList.txt -headerFile %~dp0..\..\netstandard\ref\license-header.txt -excludeApiList %~dp0apiExcludeList.txt
--- platforms/xamarin.ios/seed.sh
+++ platforms/xamarin.ios/seed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SeedAssemblies="mscorlib.dll;System.dll;System.Core.dll;System.ComponentModel.Composition.dll;System.Data.dll;System.IO.Compression.dll;System.IO.Compression.FileSystem.dll;System.Net.Http.dll;System.Numerics.dll;System.Runtime.Serialization.dll;System.Transactions.dll;System.Xml.dll;System.Xml.Linq.dll;System.Web.Services.dll;OpenTK-1.0.dll;Xamarin.iOS.dll"
+SeedAssemblies="mscorlib.dll;System.dll;System.Core.dll;System.ComponentModel.Composition.dll;System.Data.dll;System.IO.Compression.dll;System.IO.Compression.FileSystem.dll;System.Net.Http.dll;System.Numerics.dll;System.Numerics.Vectors.dll;System.Runtime.Serialization.dll;System.Transactions.dll;System.Xml.dll;System.Xml.Linq.dll;System.Web.Services.dll;OpenTK-1.0.dll;Xamarin.iOS.dll"
 
 $ScriptDir/../../Tools/dotnetcli/dotnet $ScriptDir/../../Tools/GenAPI.exe -assembly $SeedAssemblies -libPath /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/lib/mono/Xamarin.iOS/ -out $ScriptDir -excludeAttributesList $ScriptDir/../attributeExcludeList.txt -headerFile $ScriptDir/../../netstandard/ref/license-header.txt -excludeApiList $ScriptDir/apiExcludeList.txt
 
