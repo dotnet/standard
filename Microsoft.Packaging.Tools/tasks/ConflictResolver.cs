8f4d3fbea8391012874e32035e9fa46f468fa394
Eric St. John <ericstj@microsoft.com>
2017-05-10 12:52:48 -0700

    Don't report a conflict between two items that don't exist
    Port of https://github.com/dotnet/sdk/commit/1e9d2cf1b61fc52aa9b031302e767c53952bc88c

--- Microsoft.Packaging.Tools/tasks/ConflictResolver.cs
+++ Microsoft.Packaging.Tools/tasks/ConflictResolver.cs
@@ -99,6 +99,12 @@ namespace Microsoft.DotNet.Build.Tasks
             var exists1 = item1.Exists;
             var exists2 = item2.Exists;
 
+            if (!exists1 && !exists2)
+            {
+                //  If neither file exists, then don't report a conflict, as both items should be resolved (or not) to the same reference assembly
+                return null;
+            }
+
             if (!exists1 || !exists2)
             {
                 var fileMessage = !exists1 ?
