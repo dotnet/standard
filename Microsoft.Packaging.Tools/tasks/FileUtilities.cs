c1eb738db0867e3b5875e79f9aff6ce14e00baad
Eric St. John <ericstj@microsoft.com>
2017-05-15 14:33:25 -0700

    Don't cross compile GetAssemblyVersion
    Previously I was cross-compiling the GetAssemblyVersion method to avoid
    a dependency on MetadataReader on desktop.  This had the downside of causing
    first chance exceptions when run on desktop.

--- Microsoft.Packaging.Tools/tasks/FileUtilities.cs
+++ Microsoft.Packaging.Tools/tasks/FileUtilities.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
 
 namespace Microsoft.DotNet.Build.Tasks
 {
@@ -31,5 +33,32 @@ namespace Microsoft.DotNet.Build.Tasks
             return s_assemblyExtensions.Contains(extension) ? GetAssemblyVersion(sourcePath) : null;
         }
 
+        private static Version GetAssemblyVersion(string sourcePath)
+        {
+            using (var assemblyStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read, FileShare.Delete | FileShare.Read))
+            {
+                Version result = null;
+                try
+                {
+                    using (PEReader peReader = new PEReader(assemblyStream, PEStreamOptions.LeaveOpen))
+                    {
+                        if (peReader.HasMetadata)
+                        {
+                            MetadataReader reader = peReader.GetMetadataReader();
+                            if (reader.IsAssembly)
+                            {
+                                result = reader.GetAssemblyDefinition().Version;
+                            }
+                        }
+                    }
+                }
+                catch (BadImageFormatException)
+                {
+                    // not a PE
+                }
+
+                return result;
+            }
+        }
     }
 }
