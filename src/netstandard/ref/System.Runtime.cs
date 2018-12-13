51cb435eebb45d57581e78f0e3146654d9897040
Immo Landwerth <immol@microsoft.com>
2018-12-13 11:49:44 -0800

    Require support for default implementations of interfaces

--- src/netstandard/ref/System.Runtime.cs
+++ src/netstandard/ref/System.Runtime.cs
@@ -4,6 +4,12 @@
 
 namespace System.Runtime
 {
+    public sealed partial class AmbiguousImplementationException : System.Exception
+    {
+        public AmbiguousImplementationException() { }
+        public AmbiguousImplementationException(string message) { }
+        public AmbiguousImplementationException(string message, System.Exception innerException) { }
+    }
     [System.AttributeUsageAttribute(System.AttributeTargets.Assembly, Inherited=false)]
     public sealed partial class AssemblyTargetedPatchBandAttribute : System.Attribute
     {
